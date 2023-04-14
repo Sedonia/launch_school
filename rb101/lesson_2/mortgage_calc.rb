daf4a3eed69f83a13627cd359b97654c2574135d
Sedonia Steininger <coolstuff@madebysedonia.com>
2023-04-14 11:47:10 -0400

    refactored mortgage loan calculator

--- rb101/lesson_2/mortgage_calc.rb
+++ rb101/lesson_2/mortgage_calc.rb
@@ -6,21 +6,92 @@ def prompt(message)
 end
 
 def valid_number?(num)
-  (valid_integer?(num) || valid_float?(num)) && positive?(num)
+  valid_integer?(num) || valid_float?(num)
 end
 
 def valid_integer?(num)
-  num.to_i.to_s == num && num != '0'
+  num.to_i.to_s == num.to_s && num.to_i >= 0
 end
 
 def valid_float?(num)
-  num.to_f.to_s == num && num != '0'
+  num.to_f.to_s == num.to_s && num.to_f >= 0
 end
 
 def positive?(num)
-  num.to_i.positive?
+  num.to_f.positive?
 end
 
+def valid_positive_number?(num)
+  valid_number?(num) && positive?(num)
+end
+
+def valid_over100?(num)
+  valid_number?(num) && positive?(num) && num.to_i >= 100
+end
+
+def invalid_under100?(num)
+  valid_number?(num) && positive?(num) && num.to_i < 100
+end
+
+def valid_under100?(num)
+  valid_number?(num) && positive?(num) && num.to_i <= 100
+end
+
+def invalid_over100?(num)
+  valid_number?(num) && positive?(num) && num.to_i > 100
+end
+
+def validate_amount
+  loan_amount = nil
+  loop do
+    prompt MESSAGES['amount']
+    loan_amount = gets.chomp.gsub(',', '').gsub('$', '')
+    if valid_over100?(loan_amount)
+      break
+    elsif invalid_under100?(loan_amount) || loan_amount.to_f == 0
+      puts MESSAGES['low_amount']
+    else
+      puts MESSAGES['error']
+    end
+  end
+  loan_amount.to_f.truncate(2)
+end
+
+def calculate_monthly_interest
+  monthly_interest = nil
+  loop do
+    prompt MESSAGES['apr']
+    apr = gets.chomp.gsub('%', '')
+    if valid_under100?(apr)
+      monthly_interest = ((apr.to_f / 12) / 100)
+      break
+    elsif invalid_over100?(apr)
+      puts MESSAGES['too_high']
+    else
+      puts MESSAGES['error']
+    end
+  end
+  monthly_interest
+end
+
+def calculate_duration
+  duration_in_months = nil
+  loop do
+    prompt MESSAGES['duration']
+    duration = gets.chomp
+    if valid_positive_number?(duration) && duration.to_f <= 35
+      duration_in_months = duration.to_f * 12
+      break
+    elsif duration.to_f > 35 && positive?(duration)
+      puts MESSAGES['too_long']
+    else
+      puts MESSAGES['error']
+    end
+  end
+  duration_in_months
+end
+
+system "clear"
 message = <<-WELCOME
 Welcome to the Mortgage Calculator!
 
@@ -40,65 +111,37 @@ puts MESSAGES['space']
 prompt MESSAGES['start']
 puts MESSAGES['space']
 
-loan_amount = nil
-monthly_interest = nil
-duration_months = nil
-monthly_payment = nil
-
 loop do
-  loop do
-    prompt MESSAGES['amount']
-    loan_amount = gets.chomp
-    if valid_number?(loan_amount)
-      break
-    else
-      puts MESSAGES['error']
-    end
-  end
+  loan_amount = validate_amount
+  monthly_interest = calculate_monthly_interest
+  apr_to_monthly = monthly_interest * 100
+  duration_in_months = calculate_duration
 
-  monthly_rate = nil
-  loop do
-    prompt MESSAGES['apr']
-    apr = gets.chomp
-    if valid_number?(apr)
-      monthly_rate = (apr.to_f / 12)
-      monthly_interest = ((apr.to_f / 12) / 100)
-      break
-    else
-      puts MESSAGES['error']
-    end
-  end
-
-  loop do
-    prompt MESSAGES['duration']
-    duration = gets.chomp
-    if valid_integer?(duration) && positive?(duration)
-      duration_months = duration.to_i * 12
-      break
-    else
-      print MESSAGES['error']
-      puts MESSAGES['whole']
-    end
-  end
-
-  monthly_payment = (loan_amount.to_i *
+  if monthly_interest.zero?
+    monthly_payment = (loan_amount.to_i / duration_in_months.to_f)
+  else
+    monthly_payment = (loan_amount.to_i *
                     (monthly_interest /
-                    (1 - (1 + monthly_interest)**(-duration_months))))
+                    (1 - (1 + monthly_interest)**(-duration_in_months))))
+  end
 
-  prompt("Great, thanks for that information!")
-  prompt("It looks like your...")
+  puts ""
+  prompt MESSAGES['thanks']
+  prompt MESSAGES['working']
   puts MESSAGES['space']
   puts MESSAGES['asterisks']
-  puts "Montly Interest Rate is #{monthly_rate.truncate(4)}%"
-  puts "Loan Duration is #{duration_months} months"
+  puts "Montly Interest Rate is #{apr_to_monthly.truncate(4)}%"
+  puts "Loan Duration is #{duration_in_months.truncate(2)} months"
   puts "Monthly Payment is $#{monthly_payment.truncate(2)}"
   puts MESSAGES['asterisks']
   puts MESSAGES['space']
   prompt MESSAGES['start_over']
   answer = gets.chomp.downcase
   break unless answer.start_with?('y')
+  system "clear"
   puts MESSAGES['space']
   puts MESSAGES['start']
 end
 
-prompt("Okay, bye!")
+prompt("Okay, thanks for using the Mortgage Calculator! Bye!")
+puts ""
