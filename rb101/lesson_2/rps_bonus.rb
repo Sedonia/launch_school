100f91952450b15e260adb577f0854982a5dae72
Sedonia Steininger <coolstuff@madebysedonia.com>
2023-04-21 21:03:46 -0400

    updated rock, paper, scissors bonus features

--- rb101/lesson_2/rps_bonus.rb
+++ rb101/lesson_2/rps_bonus.rb
@@ -1,35 +1,55 @@
 require 'yaml'
 MESSAGES = YAML.load_file('rps.yml')
 
-VALID_CHOICES = %w(rock paper scissors lizard Spock)
-
-WINNERS = { rock: ['lizard', 'scissors'], scissors: ['paper', 'lizard'],
-            paper: ['rock', 'Spock'], lizard: ['Spock', 'paper'],
-            Spock: ['scissors', 'rock'] }
+VALID_CHOICES = { 'rock' => 'r', 'paper' => 'p', 'scissors' => 'sc',
+                  'lizard' => 'l', 'spock' => 'sp' }
 
 MEANS = { cuts: ['scissors', 'paper'], covers: ['paper', 'rock'],
-          squashes: ['rock', 'lizard'], poisons: ['lizard', 'Spock'],
-          smashes: ['Spock', 'scissors'], decapitates: ['scissors', 'lizard'],
-          eats: ['lizard', 'paper'], disproves: ['paper', 'Spock'],
-          vaporizes: ['Spock', 'rock'], crushes: ['rock', 'scissors'] }
+          squashes: ['rock', 'lizard'], poisons: ['lizard', 'spock'],
+          smashes: ['spock', 'scissors'], decapitates: ['scissors', 'lizard'],
+          eats: ['lizard', 'paper'], disproves: ['paper', 'spock'],
+          vaporizes: ['spock', 'rock'], crushes: ['rock', 'scissors'] }
+
+NUMBER_TO_WIN = 3
 
 def prompt(message)
   puts "=> #{message}"
 end
 
 def valid_play?(user_input)
-  WINNERS.include?(user_input.to_sym)
+  VALID_CHOICES.value?(user_input) || VALID_CHOICES.key?(user_input)
+end
+
+def prints_play(input)
+  if VALID_CHOICES.value?(input)
+    puts VALID_CHOICES.key(input)
+  else
+    puts input
+  end
+end
+
+def usable_play(input)
+  if VALID_CHOICES.value?(input)
+    VALID_CHOICES.key(input)
+  else
+    input
+  end
+end
+
+def says_play_valid(input)
+  if valid_play?(input)
+    print MESSAGES['player_pick']
+    prints_play(input)
+  else
+    puts MESSAGES['invalid_entry']
+  end
 end
 
 def player_wins?(play)
   MEANS.value?(play)
 end
 
-def computer_wins?(play)
-  MEANS.value?(play)
-end
-
-def says_how_defeated(hsh)
+def says_method(hsh)
   method_of_defeat = MEANS.key(hsh)
   method_of_defeat.to_s
 end
@@ -40,74 +60,50 @@ Welcome to Rock, Paper, Scissors, Lizard, Spock!
     You'll be playing against the computer.
     The first to reach three wins takes the title of <<GRAND WINNER>>!
 
-WELCOME
-
-rules = <<-RULES
-Here are the rules:
+=> Here are the rules:
     Scissors > Paper; Paper > Rock;
     Rock > Lizard; Lizard > Spock;
     Spock > Scissors; Scissors > Lizard;
     Lizard > Paper; Paper > Spock;
     Spock > Rock; Rock > Scissors
 
-Example: You play Spock, the computer plays rock. You win because Spock vaporizes rock!
+=> Example: You play Spock, the computer plays rock. You win because Spock vaporizes rock!
 
-Easy peasy. Okay, let's PLAY!
+=> Easy peasy. Okay, let's PLAY!
 
-RULES
+WELCOME
 
 system "clear"
 prompt message
-prompt rules
 
 plyr_cntr = 0
 cmp_cntr = 0
-answer = ''
 
 loop do
+  answer = nil
   loop do
     prompt MESSAGES['play']
     answer = gets.chomp.downcase
-
-    case answer
-    when '1'
-      answer = "rock"
-    when '2'
-      answer = 'paper'
-    when '3'
-      answer = 'scissors'
-    when '4'
-      answer = 'lizard'
-    when '5'
-      answer = 'Spock'
-    else
-      answer = 'wrong'
-    end
-
-    if valid_play?(answer)
-      puts MESSAGES['player_pick'] + answer
-      break
-    else
-      puts "Oops, that's not a valid entry."
-    end
+    says_play_valid(answer)
+    break if valid_play?(answer)
   end
 
-  computer_choice = VALID_CHOICES.sample
+  cmp_choice = VALID_CHOICES.keys.sample
   print MESSAGES['computer_pick']
-  puts computer_choice
+  puts cmp_choice
 
-  user_wins = [answer, computer_choice]
-  computer_wins = [computer_choice, answer]
-  says_how_defeated(MEANS)
+  user_wins = [usable_play(answer), cmp_choice]
+  comp_wins = [cmp_choice, usable_play(answer)]
+  says_method(MEANS)
 
   if player_wins?(user_wins)
     plyr_cntr += 1
-    prompt "#{answer} #{says_how_defeated(user_wins)} #{computer_choice}"
+    prompt "#{usable_play(answer)} #{says_method(user_wins)} #{cmp_choice}"
     prompt MESSAGES['winning']
     prompt "That's #{plyr_cntr} to you and #{cmp_cntr} to the computer."
-  elsif MEANS.value?(computer_wins)
+  elsif MEANS.value?(comp_wins)
     cmp_cntr += 1
-    prompt "#{computer_choice} #{says_how_defeated(computer_wins)} #{answer}"
+    prompt "#{cmp_choice} #{says_method(comp_wins)} #{usable_play(answer)}"
     prompt MESSAGES['losing']
     prompt "That's #{cmp_cntr} to the computer and #{plyr_cntr} to you."
   else
@@ -117,7 +113,7 @@ loop do
 
   puts MESSAGES['space']
 
-  if plyr_cntr == 3
+  if plyr_cntr == NUMBER_TO_WIN
     prompt MESSAGES['you_win']
     prompt MESSAGES['again?']
     replay = gets.chomp.downcase
@@ -125,7 +121,7 @@ loop do
     plyr_cntr = 0
     cmp_cntr = 0
     system 'clear'
-  elsif cmp_cntr == 3
+  elsif cmp_cntr == NUMBER_TO_WIN
     prompt MESSAGES['computer_wins']
     prompt MESSAGES['again?']
     replay = gets.chomp.downcase
