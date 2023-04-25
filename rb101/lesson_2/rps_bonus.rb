6101a92cc2ecaa2df6c601b65aa881597220f03e
Sedonia Steininger <coolstuff@madebysedonia.com>
2023-04-24 21:25:14 -0400

    updated rock, paper, scissors bonus features

--- rb101/lesson_2/rps_bonus.rb
+++ rb101/lesson_2/rps_bonus.rb
@@ -28,7 +28,7 @@ def prints_play(input)
   end
 end
 
-def usable_play(input)
+def format_play(input)
   if VALID_CHOICES.value?(input)
     VALID_CHOICES.key(input)
   else
@@ -36,7 +36,7 @@ def usable_play(input)
   end
 end
 
-def says_play_valid(input)
+def prints_player_choice(input)
   if valid_play?(input)
     print MESSAGES['player_pick']
     prints_play(input)
@@ -49,86 +49,81 @@ def player_wins?(play)
   MEANS.value?(play)
 end
 
-def says_method(hsh)
+def decide_method(hsh)
   method_of_defeat = MEANS.key(hsh)
   method_of_defeat.to_s
 end
 
-message = <<-WELCOME
-Welcome to Rock, Paper, Scissors, Lizard, Spock!
-    A twist on the classic game of rock, paper, scissors.
-    You'll be playing against the computer.
-    The first to reach three wins takes the title of <<GRAND WINNER>>!
+def prints_win_message(input, player, computer)
+  prompt "#{format_play(input)} #{decide_method(player)} #{computer}"
+  prompt MESSAGES['winning']
+end
 
-=> Here are the rules:
-    Scissors > Paper; Paper > Rock;
-    Rock > Lizard; Lizard > Spock;
-    Spock > Scissors; Scissors > Lizard;
-    Lizard > Paper; Paper > Spock;
-    Spock > Rock; Rock > Scissors
+def prints_lose_message(input, computer, player)
+  prompt "#{input} #{decide_method(computer)} #{format_play(player)}"
+  prompt MESSAGES['losing']
+end
 
-=> Example: You play Spock, the computer plays rock. You win because Spock vaporizes rock!
+def prints_count(player, computer)
+  prompt "That's #{player} to you and #{computer} to the computer."
+end
 
-=> Easy peasy. Okay, let's PLAY!
+def play_again?
+  prompt MESSAGES['again?']
+  replay = gets.chomp.downcase
+  replay.start_with?('y')
+end
 
-WELCOME
+def clear_board
+  system "clear"
+end
 
-system "clear"
-prompt message
+clear_board
+prompt MESSAGES['welcome']
 
-plyr_cntr = 0
-cmp_cntr = 0
+player_counter = 0
+computer_counter = 0
 
 loop do
   answer = nil
   loop do
     prompt MESSAGES['play']
     answer = gets.chomp.downcase
-    says_play_valid(answer)
+    prints_player_choice(answer)
     break if valid_play?(answer)
   end
 
-  cmp_choice = VALID_CHOICES.keys.sample
+  comp_choice = VALID_CHOICES.keys.sample
   print MESSAGES['computer_pick']
-  puts cmp_choice
+  puts comp_choice
 
-  user_wins = [usable_play(answer), cmp_choice]
-  comp_wins = [cmp_choice, usable_play(answer)]
-  says_method(MEANS)
+  user_wins = [format_play(answer), comp_choice]
+  comp_wins = [comp_choice, format_play(answer)]
 
   if player_wins?(user_wins)
-    plyr_cntr += 1
-    prompt "#{usable_play(answer)} #{says_method(user_wins)} #{cmp_choice}"
-    prompt MESSAGES['winning']
-    prompt "That's #{plyr_cntr} to you and #{cmp_cntr} to the computer."
+    player_counter += 1
+    clear_board
+    prints_win_message(answer, user_wins, comp_choice)
   elsif MEANS.value?(comp_wins)
-    cmp_cntr += 1
-    prompt "#{cmp_choice} #{says_method(comp_wins)} #{usable_play(answer)}"
-    prompt MESSAGES['losing']
-    prompt "That's #{cmp_cntr} to the computer and #{plyr_cntr} to you."
+    computer_counter += 1
+    clear_board
+    prints_lose_message(comp_choice, comp_wins, answer)
   else
+    clear_board
     prompt MESSAGES['tie']
-    prompt "Still #{plyr_cntr} to you and #{cmp_cntr} to the computer."
   end
 
+  prints_count(player_counter, computer_counter)
   puts MESSAGES['space']
 
-  if plyr_cntr == NUMBER_TO_WIN
+  if player_counter == NUMBER_TO_WIN
     prompt MESSAGES['you_win']
-    prompt MESSAGES['again?']
-    replay = gets.chomp.downcase
-    break unless replay.start_with?('y')
-    plyr_cntr = 0
-    cmp_cntr = 0
-    system 'clear'
-  elsif cmp_cntr == NUMBER_TO_WIN
+    player_counter = 0 && computer_counter = 0
+    break unless play_again?
+  elsif computer_counter == NUMBER_TO_WIN
     prompt MESSAGES['computer_wins']
-    prompt MESSAGES['again?']
-    replay = gets.chomp.downcase
-    break unless replay.start_with?('y')
-    plyr_cntr = 0
-    cmp_cntr = 0
-    system 'clear'
+    player_counter = 0 && computer_counter = 0
+    break unless play_again?
   end
 end
 
